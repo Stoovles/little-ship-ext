553dfaa8310c91f2c52c909674f66dabbc7c01fc
Ethan Grab <ethangrab@gmail.com>
2019-04-16 17:34:33 -0600

    Small changes for testing

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,6 +1,5 @@
 class Coupon < ApplicationRecord
   belongs_to :user
-  belongs_to :order, optional: true
   validates_presence_of :name, :discount
   validates_uniqueness_of :name
   validates :discount, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than_or_equal_to: 100 }
@@ -9,7 +8,7 @@ class Coupon < ApplicationRecord
 
 
   def self.find_coupons(cart)
-    where(user_id: [cart.items.pluck(:user_id)]).pluck(:name)
+    where(user_id: [cart.items.pluck(:user_id)]).where(enabled: true).pluck(:name)
   end
 
 
